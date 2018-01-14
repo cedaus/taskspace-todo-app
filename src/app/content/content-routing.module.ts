e49aa061e89d618e6c57bc67b7e4ae6764c38286
Saksham <cedaus97@gmail.com>
2018-01-15 00:08:03 +0530

    minor fix

--- src/app/content/content-routing.module.ts
+++ src/app/content/content-routing.module.ts
@@ -1,15 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // PROJECT
-import {ContentComponent} from './content.component';
 
 const routes: Routes = [
-  {
-    path: 'content',
-    children: [
-      { path: 'home', component: ContentComponent},
-    ]
-  },
 ];
 
 @NgModule({
