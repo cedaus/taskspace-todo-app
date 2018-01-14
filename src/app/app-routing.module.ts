0ccfddb460e85e9961d905994a3277cee7fa1d22
Saksham <cedaus97@gmail.com>
2018-01-15 00:02:31 +0530

    minor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from './_shared/login/login.component';
 
 const routes: Routes = [
+  { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
 ];
 
